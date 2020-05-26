d32fd699b1c251b0d0b514e2181778d515021df3
Ian Castillo <iancastillo@Ians-MacBook-Pro.local>
2020-05-25 23:01:30 -0500

    MVP

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code away!
+// import server
+const server = require('./server');
+require('dotenv').config();
+
+const port = process.env.PORT || 5001;
+// listen on port 4000
+server.listen(port, () => {
+    console.log(`listening on port ${port}`);
+})
